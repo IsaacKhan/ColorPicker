cd25e21a80e0f1606721ef5a3542efcefc12ac9b
Isaac Khan <36248421+IsaacKhan@users.noreply.github.com>
2022-02-15 23:10:26 -0500

    Why was this a separate change GitHub

--- app/src/main/java/com/goldenhour/colorpicker/ui/screens/colorpicker/COLOR_PICKER_DATA_PROVIDER.kt
+++ app/src/main/java/com/goldenhour/colorpicker/ui/screens/colorpicker/COLOR_PICKER_DATA_PROVIDER.kt
@@ -28,8 +28,8 @@ import kotlin.math.roundToInt
 
 @InternalCoroutinesApi
 @ExperimentalPagerApi
-class COLOR_PICKER_DATA_PROVIDER: ViewModel() {
-    companion object{
+class COLOR_PICKER_DATA_PROVIDER : ViewModel() {
+    companion object {
         const val COLORS_PER_PAGE = 10
         const val COLORS_PER_ROW = 12
         const val INITIAL_COLOR = "1"
@@ -48,22 +48,132 @@ class COLOR_PICKER_DATA_PROVIDER: ViewModel() {
     // Grid Colors
     val grid_color_list = listOf(
         // White to Black
-        "#FFFFFF", "#EEEEEE", "#CCCCCC", "#BBBBBB", "#AAAAAA", "#999999", "#888888", "#777777", "#666666", "#333333", "#111111",  "#000000",
+        "#FFFFFF",
+        "#EEEEEE",
+        "#CCCCCC",
+        "#BBBBBB",
+        "#AAAAAA",
+        "#999999",
+        "#888888",
+        "#777777",
+        "#666666",
+        "#333333",
+        "#111111",
+        "#000000",
 
         // Darker
-        "#009696", "#000096", "#800096", "#960096", "#960000", "#968000", "#96C500", "#96D500", "#969600", "#809600", "#009600",  "#009680",
-        "#00AFAF", "#0000AF", "#8000AF", "#AF00AF", "#AF0000", "#AF8000", "#AFC500", "#AFD500", "#AFAF00", "#80AF00", "#00AF00",  "#00AF80",
-        "#00C8C8", "#0000C8", "#8000C8", "#C800C8", "#C80000", "#C88000", "#C8C500", "#C8D500", "#C8C800", "#80C800", "#00C800",  "#00C880",
-        "#00E6E6", "#0000E6", "#8000E6", "#E600E6", "#E60000", "#E68000", "#E6C500", "#E6D500", "#E6E600", "#80E600", "#00E600",  "#00E680",
+        "#009696",
+        "#000096",
+        "#800096",
+        "#960096",
+        "#960000",
+        "#968000",
+        "#96C500",
+        "#96D500",
+        "#969600",
+        "#809600",
+        "#009600",
+        "#009680",
+        "#00AFAF",
+        "#0000AF",
+        "#8000AF",
+        "#AF00AF",
+        "#AF0000",
+        "#AF8000",
+        "#AFC500",
+        "#AFD500",
+        "#AFAF00",
+        "#80AF00",
+        "#00AF00",
+        "#00AF80",
+        "#00C8C8",
+        "#0000C8",
+        "#8000C8",
+        "#C800C8",
+        "#C80000",
+        "#C88000",
+        "#C8C500",
+        "#C8D500",
+        "#C8C800",
+        "#80C800",
+        "#00C800",
+        "#00C880",
+        "#00E6E6",
+        "#0000E6",
+        "#8000E6",
+        "#E600E6",
+        "#E60000",
+        "#E68000",
+        "#E6C500",
+        "#E6D500",
+        "#E6E600",
+        "#80E600",
+        "#00E600",
+        "#00E680",
 
         //   Cyan,      Blue,    Purple,      Pink,       Red,    Orange, Y. Orange,      Gold,    Yellow,  Y. Green,     Green,  Sea Green,
-        "#00FFFF", "#0000FF", "#8000FF", "#FF00FF", "#FF0000", "#FF8000", "#FFC500", "#FFD500", "#FFFF00", "#80FF00", "#00FF00",  "#00FF80",
+        "#00FFFF",
+        "#0000FF",
+        "#8000FF",
+        "#FF00FF",
+        "#FF0000",
+        "#FF8000",
+        "#FFC500",
+        "#FFD500",
+        "#FFFF00",
+        "#80FF00",
+        "#00FF00",
+        "#00FF80",
 
         // Lighter
-        "#64FFFF", "#6464FF", "#8064FF", "#FF64FF", "#FF6464", "#FF8064", "#FFC564", "#FFD564", "#FFFF64", "#80FF64", "#64FF64",  "#64FF80",
-        "#7DFFFF", "#7D7DFF", "#807DFF", "#FF7DFF", "#FF7D7D", "#FF807D", "#FFC57D", "#FFD57D", "#FFFF7D", "#80FF7D", "#7DFF7D",  "#7DFF80",
-        "#96FFFF", "#9696FF", "#8096FF", "#FF96FF", "#FF9696", "#FF8096", "#FFC596", "#FFD596", "#FFFF96", "#80FF96", "#96FF96",  "#96FF80",
-        "#AFFFFF", "#AFAFFF", "#80AFFF", "#FFAFFF", "#FFAFAF", "#FF80AF", "#FFC5AF", "#FFD5AF", "#FFFFAF", "#80FFAF", "#AFFFAF",  "#AFFF80",
+        "#64FFFF",
+        "#6464FF",
+        "#8064FF",
+        "#FF64FF",
+        "#FF6464",
+        "#FF8064",
+        "#FFC564",
+        "#FFD564",
+        "#FFFF64",
+        "#80FF64",
+        "#64FF64",
+        "#64FF80",
+        "#7DFFFF",
+        "#7D7DFF",
+        "#807DFF",
+        "#FF7DFF",
+        "#FF7D7D",
+        "#FF807D",
+        "#FFC57D",
+        "#FFD57D",
+        "#FFFF7D",
+        "#80FF7D",
+        "#7DFF7D",
+        "#7DFF80",
+        "#96FFFF",
+        "#9696FF",
+        "#8096FF",
+        "#FF96FF",
+        "#FF9696",
+        "#FF8096",
+        "#FFC596",
+        "#FFD596",
+        "#FFFF96",
+        "#80FF96",
+        "#96FF96",
+        "#96FF80",
+        "#AFFFFF",
+        "#AFAFFF",
+        "#80AFFF",
+        "#FFAFFF",
+        "#FFAFAF",
+        "#FF80AF",
+        "#FFC5AF",
+        "#FFD5AF",
+        "#FFFFAF",
+        "#80FFAF",
+        "#AFFFAF",
+        "#AFFF80",
     )
 
 
@@ -101,27 +211,30 @@ class COLOR_PICKER_DATA_PROVIDER: ViewModel() {
         }
     }
 
-    private fun initializeData(){
+    private fun initializeData() {
         savedColorSet.value = SHARED_PREFERENCES_MANAGER.shared.getSavedColorSet().toSet()
         colorListToDisplay.value = setColorSetToDisplay(visiblePageIndex)
         pageCount = calculatePageCount()
     }
-    private suspend fun observeCurrentPage(){
-        snapshotFlow { pagerState.currentPage }.collect(object: FlowCollector<Int>{
+
+    private suspend fun observeCurrentPage() {
+        snapshotFlow { pagerState.currentPage }.collect(object : FlowCollector<Int> {
             override suspend fun emit(value: Int) {
                 visiblePageIndex = value
                 colorListToDisplay.value = setColorSetToDisplay(value)
             }
         })
     }
+
     private fun calculatePageCount(): Int {
         return if (savedColorSet.value.size > COLORS_PER_PAGE - 1)
             (savedColorSet.value.size / COLORS_PER_PAGE) + 1
         else 1
     }
+
     private fun setInitialState(color_to_update: Int) {
         viewModelScope.launch {
-            when(color_to_update){
+            when (color_to_update) {
                 1 -> {
                     redReadOnly = false
                     redTextField = ""
@@ -140,6 +253,7 @@ class COLOR_PICKER_DATA_PROVIDER: ViewModel() {
             }
         }
     }
+
     private fun updatedSavedColorSet(colorAsHex: String): Set<String> = when {
         savedColorSet.value.first().isEmpty() -> {
             mutableSetOf(colorAsHex)
@@ -164,23 +278,31 @@ class COLOR_PICKER_DATA_PROVIDER: ViewModel() {
         }
     }
 
-    fun setReadOnly(color_to_update: Int, read_only: Boolean){
+    fun setReadOnly(color_to_update: Int, read_only: Boolean) {
         viewModelScope.launch {
-            when(color_to_update){
-                1 -> { redReadOnly = read_only }
-                2 -> { greenReadOnly = read_only }
-                3 -> { blueReadOnly = read_only }
+            when (color_to_update) {
+                1 -> {
+                    redReadOnly = read_only
+                }
+                2 -> {
+                    greenReadOnly = read_only
+                }
+                3 -> {
+                    blueReadOnly = read_only
+                }
             }
         }
     }
-    fun setCurrentDisplayColor(color: Color){
+
+    fun setCurrentDisplayColor(color: Color) {
         _colorToDisplay.value = color
     }
+
     fun setColorSetToDisplay(index: Int): List<String> {
         return savedColorSet.value.chunked(COLORS_PER_PAGE)[index]
     }
 
-    fun updateColorListToDisplay(){
+    fun updateColorListToDisplay() {
         viewModelScope.launch {
             val color = colorToDisplay.value.toArgb()
             val colorAsHex = java.lang.String.format(
@@ -192,19 +314,22 @@ class COLOR_PICKER_DATA_PROVIDER: ViewModel() {
 
             savedColorSet.value = updatedSavedColorSet(colorAsHex)
 
-            colorListToDisplay.value = savedColorSet.value.chunked(COLORS_PER_PAGE)[visiblePageIndex]
+            colorListToDisplay.value =
+                savedColorSet.value.chunked(COLORS_PER_PAGE)[visiblePageIndex]
             SHARED_PREFERENCES_MANAGER.shared.setSavedColorSet(savedColorSet.value)
             pageCount = calculatePageCount()
         }
     }
+
     fun updateTabIndex(index: Int) {
         viewModelScope.launch {
             selectedTabIndex.value = index
         }
     }
+
     fun updateColorFromSlider(color_to_update: Int, read_only: Boolean, position: Float) {
         viewModelScope.launch {
-            when(color_to_update){
+            when (color_to_update) {
                 1 -> {
                     redReadOnly = read_only
                     redPosition = position
@@ -247,9 +372,10 @@ class COLOR_PICKER_DATA_PROVIDER: ViewModel() {
             }
         }
     }
+
     fun updateColorFromTextField(color_to_update: Int, text: String) {
         viewModelScope.launch {
-            if(text.isNotEmpty()) {
+            if (text.isNotEmpty()) {
                 if (text.isNumber() && text.toFloat() in 1f..255f) {
                     when (color_to_update) {
                         1 -> {
@@ -287,8 +413,7 @@ class COLOR_PICKER_DATA_PROVIDER: ViewModel() {
                         }
                     }
                 }
-            }
-            else setInitialState(color_to_update)
+            } else setInitialState(color_to_update)
         }
     }
 }
